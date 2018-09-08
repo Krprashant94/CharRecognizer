e23c5b2499ebc4c502d7cfcb57ad4089bf8726a9
Prashant Kumar <kr.prashant94@gmail.com>
2018-09-09 01:20:15 +0530

    .

--- Feature training.py
+++ Feature training.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 from keras.models import Sequential, load_model
@@ -18,7 +18,7 @@ import os
 import re
 
 
-# In[143]:
+# In[ ]:
 
 
 class Nural_network:
@@ -41,6 +41,7 @@ class Nural_network:
         
 #         Data
         self.train_data, self.train_labels = [],[]
+        self.valid_data, self.valid_labels = [],[]
         print("__init__")
     
     def getTrainingData(self, root, i):
@@ -55,6 +56,19 @@ class Nural_network:
         lab = f.read()
         self.train_labels = np.array(eval(lab))
         f.close()
+
+    def getValidData(self, root):
+#         read training file; feature data
+        f = open(root+os.sep+"data_1", 'r')
+        val = f.read()
+        self.valid_data = np.array(eval(val))
+        f.close()
+        
+#         corresponding label 
+        f = open(root+os.sep+"lable_1", 'r')
+        lab = f.read()
+        self.valid_labels = np.array(eval(lab))
+        f.close()
         
     def model(self):
 #         TODO: Need to improve model
@@ -62,9 +76,9 @@ class Nural_network:
         if not self.isModel:
             self.isModel = True
             self.model = Sequential()
-            self.model.add(Conv2D(128, (2, 2),  input_shape=(5, 2, 6), activation='relu'))
-            self.model.add(Reshape(target_shape=((8, 64)), name='reshape'))
-            self.model.add(LSTM(64, return_sequences=True))
+            self.model.add(Conv2D(256, (2, 2),  input_shape=(5, 2, 6), activation='relu'))
+            self.model.add(Reshape(target_shape=((8, 128)), name='reshape'))
+            self.model.add(LSTM(128, return_sequences=True))
             self.model.add(Flatten())
             self.model.add(Dense(self.class_count, activation='softmax'))
             self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
@@ -77,26 +91,30 @@ class Nural_network:
     
     def Train(self, path='extracted'):
         self.isTrained = True
-        for i in range(1, 127):
+        for i in range(1, 9):
 #             Total 64 file 1016/8 = 127
             print("Trainning Set : "+str(i))
     
 #             Load the training data 
             self.getTrainingData(path, str(i))
+            self.getValidData(path)
+            
             self.train_data = self.__shape(self.train_data)
+            self.valid_data = self.__shape(self.valid_data)
             print(self.train_data.shape)
             
-            self.model.fit(self.train_data, self.train_labels, epochs=100, batch_size=10)
+            self.model.fit(self.train_data, self.train_labels, validation_data=(self.valid_data, self.valid_labels), epochs=10, batch_size=10)
+            self.save()
         
-    def load(self):
+    def load(self, path="feature.bin"):
 #         Load the saved model
         self.isTrained = True
         self.isModel = True
-        self.model = load_model("feature.bin")
+        self.model = load_model(path)
         
-    def save(self):
+    def save(self, path="feature.bin"):
 #         Save the model
-        self.model.save("feature.bin")
+        self.model.save(path)
     
     def __key_func(self, x):
 #         Function for sorting file in directory python "os"
@@ -107,32 +125,33 @@ class Nural_network:
         return "{:>10}".format(mat.group(1)) # right align to 10 digits.
 
 
-# In[144]:
+# In[ ]:
 
 
 nn = Nural_network()
 
 
-# In[145]:
+# In[ ]:
 
 
-# nn.load()
-nn.model()
+nn.load()
+# nn.model()
 
 
-# In[146]:
+# In[ ]:
 
-
-nn.Train()
+for i in range(1000):
+    print("Loop : "+str(i))
+    nn.Train()
 
 
 # In[ ]:
 
 
-nn.save()
+# nn.save()
 
 
-# In[138]:
+# In[ ]:
 
 
 print(nn.model.summary())
